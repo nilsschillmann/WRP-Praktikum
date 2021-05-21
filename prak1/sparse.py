26bcad7da43f20eec1ade91a289009ef4b9a9aa3
Nils Schillmann <nilsschillmann@googlemail.com>
2021-05-21 10:50:21 +0200

    implement the sparse matrix and tests

--- prak1/sparse.py
+++ prak1/sparse.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+# TODO: function documention
+# TODO: mat multiplication with @ operator
+#       mat @ vec with vec with 0 line? see pep
+
 
 class SparseMatrix:
     """
@@ -24,7 +28,7 @@ class SparseMatrix:
         Col indices of elements.
     """
     
-    def __init__(self, data, row, col, size=None):
+    def __init__(self, data, row, col, shape=None):
         
         #### validate the input ####
 
@@ -35,42 +39,80 @@ class SparseMatrix:
         # no negative indizes?
         if any([ x<0 or y<0 for x, y in zip(row, col)]):
             raise ValueError('indices must be positive!')
-        # correct size?
-        if size == None:
-            self.size = (max(row)+1, max(col)+1)
+        
+        # correct shape?
+        if shape == None:
+            self.shape = (max(row)+1, max(col)+1)
         else:
-            assert len(size) == 2
-            assert type(size[0]) == type(size[1]) == int
-            assert size[0] > 0
-            assert size[1] > 0
-
-            self.size = size
-
-        self.data = data
-        self.row = row
-        self.col = col
+            assert len(shape) == 2
+            assert type(shape[0]) == type(shape[1]) == int
+            assert shape[0] > 0
+            assert shape[1] > 0
+            
+            self.shape = shape
 
         self.content = dict()
         for d, r, c in zip(data, row, col):
-            self.content[(r, c)] = d
+            if d != 0:
+                self.content[(r, c)] = d
     
-    def get(self, r, c):
-        if not ((0 <= r <= self.size[0]-1) and (0 <= c <= self.size[1]-1)):
-            raise IndexError('Index out of matrix size')
+    def __getitem__(self, index):
+        self.__check_index(index)
+        return self.content.get(index, 0)
+    
+    def __setitem__(self, index, value):
+        self.__check_index(index)
+        if value != 0:
+            self.content[index] = value
         
-        return self.content.get((r, c), 0)
+    def __check_index(self, index):
+        if type(index) != tuple or len(index) != 2:
+            raise TypeError('Index must be a tuple like (x, y)')
+        if type(index[0]) != int or type(index[1]) != int:
+            raise TypeError('Index values must be of type int')
+        if not ((0 <= index[0] <= self.shape[0]-1) and (0 <= index[1] <= self.shape[1]-1)):
+            raise IndexError('Index out of matrix shape')
     
 
     def __repr__(self) -> str:
         return str(self.content)
 
+    def __eq__(self, o: object) -> bool:
+        return self.shape == o.shape and self.content == o.content
+
     def __add__(self, other):
-        pass
+        if self.shape != other.shape:
+            raise ArithmeticError('Matrizes must have the same shape!')
+        res = SparseMatrix([], [], [], shape=self.shape)
+        content = {}
+        for key in set(list(self.content.keys()) + list(other.content.keys())):
+            content[key] = self.content.get(key, 0) + other.content.get(key, 0)
+        res.content = content
+        return res
+    
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        return self.__add__(other)
+            
 
-    def __mul__(self, other):
-        pass
+    def __matmul__(self, other):
+        res = SparseMatrix([], [], [], shape=(self.shape[0], other.shape[1]))
+        
+        combinations = [(i, k) for i in range(self.shape[0]) for k in range(other.shape[1])]
+        
+        for i, k in combinations:
+            res[i, k] = sum(self[i, j] * other[j, k] for j in range(self.shape[1]))
+        
+        return res
+    
+    @property
+    def T(self):
+        shape = tuple(reversed(self.shape))
+        content = dict()
+        for key, value in self.content.items():
+            content[tuple(reversed(key))] = value
+        res = SparseMatrix([], [], [], shape=shape)
+        res.content = content
+        return res
 
-
-if __name__ == '__main__':
-    mat = SparseMatrix()
-    print(type(mat))
--- prak1/test.py
+++ prak1/test.py
@@ -4,6 +4,7 @@ from unittest import main
 from sparse import SparseMatrix
 
 
+
 class TestSparse(unittest.TestCase):
 
     def test_input_same_length(self):
@@ -21,10 +22,10 @@ class TestSparse(unittest.TestCase):
         col    = [1, 2, 3, 4]
         mat = SparseMatrix(values, row, col)
 
-        self.assertEqual(mat.get(1, 1), 1)
-        self.assertEqual(mat.get(2, 2), 2)
-        self.assertEqual(mat.get(3, 3), 3)
-        self.assertEqual(mat.get(4, 4), 4)
+        self.assertEqual(mat[1, 1], 1)
+        self.assertEqual(mat[2, 2], 2)
+        self.assertEqual(mat[3, 3], 3)
+        self.assertEqual(mat[4, 4], 4)
 
     def test_zero_indexing(self):
         values = [1, 2, 3, 4]
@@ -32,10 +33,10 @@ class TestSparse(unittest.TestCase):
         col    = [1, 2, 3, 4]
         mat = SparseMatrix(values, row, col)
 
-        self.assertEqual(mat.get(0, 0), 0)
-        self.assertEqual(mat.get(1, 2), 0)
-        self.assertEqual(mat.get(3, 4), 0)
-        self.assertEqual(mat.get(4, 3), 0)
+        self.assertEqual(mat[0, 0], 0)
+        self.assertEqual(mat[1, 2], 0)
+        self.assertEqual(mat[3, 4], 0)
+        self.assertEqual(mat[4, 3], 0)
 
     def test_out_of_range_indexing(self):
         values = [1, 2, 3, 4]
@@ -43,25 +44,123 @@ class TestSparse(unittest.TestCase):
         col    = [1, 2, 3, 4]
         mat = SparseMatrix(values, row, col)
 
-        self.assertRaises(IndexError, mat.get, 0, -1)
-        self.assertRaises(IndexError, mat.get, -1, 0)
-        self.assertRaises(IndexError, mat.get, 0, 5)
-        self.assertRaises(IndexError, mat.get, 5, 0)
+        self.assertRaises(IndexError, mat.__getitem__, (0, -1))
+        self.assertRaises(IndexError, mat.__getitem__, (-1, 0))
+        self.assertRaises(IndexError, mat.__getitem__, (0, 5))
+        self.assertRaises(IndexError, mat.__getitem__, (5, 0))
 
-    def test_size_parameter(self):
+    def test_shape_parameter(self):
         values = [1, 2]
         row    = [0, 1]
         col    = [0, 1]
         a = SparseMatrix(values, row, col)
-        b = SparseMatrix(values, row, col, size=(3, 4))
+        b = SparseMatrix(values, row, col, shape=(3, 4))
         c = SparseMatrix([1, 1], [12, 23], [7, 9])
 
-        self.assertEqual(a.size, (2, 2))
-        self.assertEqual(b.size, (3, 4))
-        self.assertRaises(IndexError, a.get, 2, 3)
-        self.assertEqual(b.get(2, 3), 0)
-        self.assertEqual(c.size, (24, 10))
+        self.assertEqual(a.shape, (2, 2))
+        self.assertEqual(b.shape, (3, 4))
+        self.assertRaises(IndexError, a.__getitem__, (2, 3))
+        self.assertEqual(b[2, 3], 0)
+        self.assertEqual(c.shape, (24, 10))
 
+    def test_equals(self):
+        a = SparseMatrix([1, 1, 1],
+                         [0, 1, 2],
+                         [0, 1, 2])
+        
+        b = SparseMatrix([1, 1, 1], 
+                         [2, 1, 0], 
+                         [2, 1, 0])
+        
+        c = SparseMatrix([1, 1, 1], 
+                         [2, 0, 1], 
+                         [2, 0, 1])
+        
+        d = SparseMatrix([1, 1, 1, 0], 
+                         [0, 1, 2, 1], 
+                         [0, 1, 2, 2])
+        
+        e = SparseMatrix([1, 1, 1],
+                         [0, 1, 2],
+                         [0, 1, 2], shape=(4, 4))
+
+        self.assertEqual(a, b)
+        self.assertEqual(a, c)
+        self.assertEqual(b, c)
+        self.assertEqual(a, d)
+        self.assertNotEqual(a, e)
+        
+        
+
+    def test_add(self):
+        a = SparseMatrix([1, 1, 1], [0, 1, 2], [0, 1, 2], shape=(3, 3))
+        b = SparseMatrix([1, 1, 1], [1, 1, 1], [0, 1, 2], shape=(3, 3))
+        c = SparseMatrix([1, 1, 2, 1, 1], 
+                         [0, 1, 1, 1, 2],
+                         [0, 0, 1, 2, 2])
+
+        d = a + b
+        
+        self.assertEqual(c, d)
+        
+        e = SparseMatrix([], [], [], shape=(10, 10))
+        self.assertRaises(ArithmeticError, a.__add__, e)
+        
+    def test_radd(self):
+        a = SparseMatrix([1, 1, 1], [0, 1, 2], [0, 1, 2], shape=(3, 3))
+        b = SparseMatrix([1, 1, 1], [1, 1, 1], [0, 1, 2], shape=(3, 3))
+        c = SparseMatrix([1, 1, 2, 1, 1], 
+                         [0, 1, 1, 1, 2],
+                         [0, 0, 1, 2, 2])
+
+        self.assertEqual(a, 0+a)
+        
+        d = sum([a, b])
+        
+        self.assertEqual(c, d)
+        
+
+    def test_transposed(self):
+        a = SparseMatrix([1, 1, 1], [0, 1, 2], [0, 1, 2])
+        self.assertEqual(a, a.T)
+        
+        b = SparseMatrix([1, 1, 1], [0, 0, 0], [0, 1, 2])
+        c = SparseMatrix([1, 1, 1], [0, 1, 2], [0, 0, 0])
+        self.assertEqual(b, c.T)
+    
+    def test_setitem(self):
+        a = SparseMatrix([1, 1, 1], [0, 1, 2], [0, 1, 2])
+        self.assertEqual(a[0,0], 1)
+        a[0,0] = 5
+        self.assertEqual(a[0,0], 5)
+        
+        self.assertEqual(a[0,2], 0)
+        a[0,2] = 5
+        self.assertEqual(a[0,2], 5)
+        
+        self.assertRaises(IndexError, a.__setitem__, (0, 3), 5)
+        
+    def test_matmul(self):
+        # test shape
+        a = SparseMatrix([], [], [], shape=(3, 5))
+        b = SparseMatrix([], [], [], shape=(5, 7))
+        self.assertEqual((3, 7), (a @ b).shape)
+        
+        
+        #example from Wikipedia
+        a = SparseMatrix([3, 2, 1, 1, 2],
+                         [0, 0, 0, 1, 1],
+                         [0, 1, 2, 0, 2])
+        b = SparseMatrix([1, 2, 1, 4],
+                         [0, 0, 1, 2],
+                         [0, 1, 1, 0])
+        c = SparseMatrix([7, 8, 9, 2], 
+                         [0, 0, 1, 1], 
+                         [0, 1, 0, 1])
+        
+        self.assertEqual(c, a @ b)
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
